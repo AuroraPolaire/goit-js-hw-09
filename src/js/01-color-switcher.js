5a909727a3ae6e6daf6fcb2b65c7e77edf1d127b
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-01-10 23:46:44 +0200

    test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,7 @@ const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let colorChangeIntId = '';
+console.log(body);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
