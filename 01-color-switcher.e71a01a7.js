8911e8bd9c49b5aeec92a23ab848ded3dee1904e
AuroraPolaire <106490296+AuroraPolaire@users.noreply.github.com>
2023-01-10 21:47:15 +0000

    Deploying to gh-pages from @ AuroraPolaire/goit-js-hw-09@5a909727a3ae6e6daf6fcb2b65c7e77edf1d127b 🚀

---
+++ 01-color-switcher.e71a01a7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body"),n="";console.log(o),t.addEventListener("click",(function(r){n=setInterval((function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(o){clearInterval(n),e.setAttribute("disabled",!0),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.e71a01a7.js.map
